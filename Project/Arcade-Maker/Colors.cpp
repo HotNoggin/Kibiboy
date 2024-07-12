36b5362fb7f08338e8a58bbfba9f8619624bd6a9
Hot Noggin Studios <143744752+HotNoggin@users.noreply.github.com>
2024-07-12 18:06:06 -0500

    Colors separated from Canvas + pixel() method

--- Project/Arcade-Maker/Colors.cpp
+++ Project/Arcade-Maker/Colors.cpp
@@ -1,5 +1,13 @@
 #include "Colors.h"
 
+
+Color::Color() {
+	r = 0;
+	g = 0;
+	b = 0;
+}
+
+
 Color::Color(uint8_t red, uint8_t green, uint8_t blue) {
 	r = red;
 	g = green;
@@ -10,3 +18,38 @@ Color::Color(uint8_t red, uint8_t green, uint8_t blue) {
 Uint32 Color::asInt(SDL_PixelFormat* format) {
 	return SDL_MapRGB(format, r, g, b);
 }
+
+
+// Color constants
+// A pure black color
+Color Colors::BLACK = Color(0x00, 0x00, 0x00);
+// A deep blue color
+Color Colors::DARK_BLUE = Color(0x1D, 0x2B, 0x53);
+// A deep purple color
+Color Colors::DARK_PURPLE = Color(0x7E, 0x25, 0x53);
+// A deep green color
+Color Colors::DARK_GREEN = Color(0x00, 0x87, 0x51);
+// A saturated brown color
+Color Colors::BROWN = Color(0xAB, 0x52, 0x36);
+// A deep gray color
+Color Colors::DARK_GRAY = Color(0x5F, 0x57, 0x4F);
+// A bright gray color
+Color Colors::LIGHT_GRAY = Color(0xC2, 0xC3, 0xC7);
+// An off-white color
+Color Colors::WHITE = Color(0xFF, 0xF1, 0xE8);
+// A bright red color
+Color Colors::RED = Color(0xFF, 0x00, 0x4D);
+// A bright orange color
+Color Colors::ORANGE = Color(0xFF, 0xA3, 0x00);
+// A bright yellow color
+Color Colors::YELLOW = Color(0xFF, 0xEC, 0x27);
+// A neon green color
+Color Colors::GREEN = Color(0x00, 0xE4, 0x36);
+// A bright blue color
+Color Colors::BLUE = Color(0x29, 0xAD, 0xFF);
+// A bright indigo color
+Color Colors::LAVENDER = Color(0x83, 0x76, 0x9C);
+// A bright pink color
+Color Colors::PINK = Color(0xFF, 0x77, 0xA8);
+// A tan/peach color
+Color Colors::PEACH = Color(0xFF, 0xCC, 0xAA);
--- Project/Arcade-Maker/Colors.h
+++ Project/Arcade-Maker/Colors.h
@@ -8,6 +8,7 @@
 struct Color {
 	int r, g, b;
 
+	Color();
 	Color(uint8_t red, uint8_t green, uint8_t blue);
 
 	// Returns the color as a single integer using the specified format
@@ -18,35 +19,35 @@ struct Color {
 namespace Colors {
 	// Color constants
 	// A pure black color
-	Color BLACK = Color(0x00, 0x00, 0x00);
+	extern Color BLACK;
 	// A deep blue color
-	Color DARK_BLUE = Color(0x1D, 0x2B, 0x53);
+	extern Color DARK_BLUE;
 	// A deep purple color
-	Color DARK_PURPLE = Color(0x7E, 0x25, 0x53);
+	extern Color DARK_PURPLE;
 	// A deep green color
-	Color DARK_GREEN = Color(0x00, 0x87, 0x51);
+	extern Color DARK_GREEN;
 	// A saturated brown color
-	Color BROWN = Color(0xAB, 0x52, 0x36);
+	extern Color BROWN;
 	// A deep gray color
-	Color DARK_GRAY = Color(0x5F, 0x57, 0x4F);
+	extern Color DARK_GRAY;
 	// A bright gray color
-	Color LIGHT_GRAY = Color(0xC2, 0xC3, 0xC7);
+	extern Color LIGHT_GRAY;
 	// An off-white color
-	Color WHITE = Color(0xFF, 0xF1, 0xE8);
+	extern Color WHITE;
 	// A bright red color
-	Color RED = Color(0xFF, 0x00, 0x4D);
+	extern Color RED;
 	// A bright orange color
-	Color ORANGE = Color(0xFF, 0xA3, 0x00);
+	extern Color ORANGE;
 	// A bright yellow color
-	Color YELLOW = Color(0xFF, 0xEC, 0x27);
+	extern Color YELLOW;
 	// A neon green color
-	Color GREEN = Color(0x00, 0xE4, 0x36);
+	extern Color GREEN;
 	// A bright blue color
-	Color BLUE = Color(0x29, 0xAD, 0xFF);
+	extern Color BLUE;
 	// A bright indigo color
-	Color LAVENDER = Color(0x83, 0x76, 0x9C);
+	extern Color LAVENDER;
 	// A bright pink color
-	Color PINK = Color(0xFF, 0x77, 0xA8);
+	extern Color PINK;
 	// A tan/peach color
-	Color PEACH = Color(0xFF, 0xCC, 0xAA);
+	extern Color PEACH;
 };
